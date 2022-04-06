0a9963f409512a4f742f12c4b983a7a93365d301
turatbek <temirbekov.tt@gmail.com>
2022-04-06 19:21:57 +0600

    Task#2

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int sum = a + b + c;
+        int times = a * b * c;
+        System.out.println("5 + 7 + 8 = " + sum);
+        System.out.println("5 * 7 * 8 = " + times);
+        System.out.println("(5 + 7 + 8) / 3 = " + sum / 3d);
+
+   }
+}
